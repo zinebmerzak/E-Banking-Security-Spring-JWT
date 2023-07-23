8778e7b06874c5e9885f673e269f5bd4c321d6d6
zinebmerzak <53410994+zinebmerzak@users.noreply.github.com>
2023-07-23 23:49:04 +0100

    Delete digital-banking-spring-backend directory

--- digital-banking-spring-backend/src/main/java/org/sid/ebankingbackend/dtos/BankAccountDTO.java
+++
@@ -1,8 +0,0 @@
-package org.sid.ebankingbackend.dtos;
-
-import lombok.Data;
-
-@Data
-public class BankAccountDTO {
-    private String type;
-}
--- digital-banking-spring-backend/src/main/java/org/sid/ebankingbackend/repositories/BankAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sid.ebankingbackend.repositories;
-
-import org.sid.ebankingbackend.entities.BankAccount;
-import org.sid.ebankingbackend.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
-}
